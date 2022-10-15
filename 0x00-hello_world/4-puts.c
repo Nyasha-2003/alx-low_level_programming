d221748f385ccfab004a0103d842cfba6ba9f7d1
Nyasha-2003 <vushenyasha279@gmail.com>
2022-10-15 01:11:56 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - print a line of code using puts
+ * main - Entry
  *
  * Return: 0
  */
@@ -8,3 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
